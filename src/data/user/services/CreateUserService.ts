458a8281fbfd78f8676c263ebe941035d0c65ca1
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-18 16:26:36 -0300

    feat: implements AbstractCreateUser

---
+++ src/data/user/services/CreateUserService.ts
@@ -0,0 +1,23 @@
+import { AbstractUserRepository } from '@/infra/repository/interfaces/AbstractUserRepository'
+import { AbstractCreateUser, User } from 'domain/usecases/CreateUser'
+import { UserEntity } from 'domain/user/UserEntity'
+
+export class CreateUserService implements AbstractCreateUser {
+	constructor(
+		private readonly userRepository: AbstractUserRepository<UserEntity, User>,
+	) {}
+
+	async execute(data: UserEntity): Promise<User> {
+		const findUserWithSameEmail = await this.userRepository.findOneByEmail(
+			data.email,
+		)
+
+		if (findUserWithSameEmail) {
+			throw new Error('Email is already used!')
+		}
+
+		const user = await this.userRepository.create(data)
+
+		return user
+	}
+}
