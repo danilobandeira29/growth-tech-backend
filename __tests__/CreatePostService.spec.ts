fd5b057a927848928814205a9ccb02619d47d46b
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-18 17:10:56 -0300

    test: add user object to create a new post

--- __tests__/CreatePostService.spec.ts
+++ __tests__/CreatePostService.spec.ts
@@ -21,8 +21,32 @@ describe('Create User Service', () => {
 		)
 	})
 	it('should be able to create a new post', async () => {
+		const user = {
+			id: 10,
+			name: 'Danilo Bandeira',
+			username: 'Bandeira',
+			email: 'danilobandeira29@gmail.com',
+			address: {
+				street: 'Kulas Light',
+				suite: 'Apt. 10',
+				city: 'Teresina',
+				zipcode: '4567489-10',
+				geo: {
+					lat: '-37.4564',
+					lng: '-37.4564',
+				},
+			},
+			phone: '(086) 32904-7310',
+			website: 'github.com',
+			company: {
+				name: 'My company',
+				catchPhase: 'Multi-layered client-server neural-net',
+				bs: 'harness real-time e-markets',
+			},
+		}
+
 		const newPost = {
-			userId: 10,
+			userId: user.id,
 			title: 'title post',
 			body: 'title body',
 		}
