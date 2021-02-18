740f01c0f3c7e18d7c1c4cab201915cbb8d80217
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-18 17:16:29 -0300

    feat: add findOneById method

--- src/infra/repository/interfaces/AbstractUserRepository.ts
+++ src/infra/repository/interfaces/AbstractUserRepository.ts
@@ -1,4 +1,5 @@
 export interface AbstractUserRepository<T, K> {
 	findOneByEmail(email: string): Promise<K | undefined>
+	findOneById(id: number): Promise<K | undefined>
 	create(data: T): Promise<K>
 }
