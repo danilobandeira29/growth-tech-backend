ecbc532e05ff38b53ea0aca1c772eb13e7fc6755
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-18 17:23:03 -0300

    test: should not be able to create a new user with that is already in use

--- __tests__/CreateUserService.spec.ts
+++ __tests__/CreateUserService.spec.ts
@@ -14,6 +14,7 @@ describe('Create User Service', () => {
 			'findOneByEmail',
 		)
 	})
+
 	it('should be able to create a new user', async () => {
 		const newUser = {
 			name: 'John Doe',
@@ -44,6 +45,63 @@ describe('Create User Service', () => {
 		expect(spyFindOneByEmailFromFakeUserRepository).toHaveBeenCalledTimes(1)
 		expect(spyFindOneByEmailFromFakeUserRepository).toHaveBeenCalledWith(
 			newUser.email,
+		)
+	})
+
+	it('should not be able to create a new user with that is already in use', async () => {
+		const newUser = {
+			name: 'John Doe',
+			username: 'Doe',
+			email: 'johndoe@test.com',
+			address: {
+				street: 'Kulas Light',
+				suite: 'Apt. 10',
+				city: 'Teresina',
+				zipcode: '4567489-10',
+				geo: {
+					lat: '-37.4564',
+					lng: '-37.4564',
+				},
+			},
+			phone: '(086) 32904-7310',
+			website: 'github.com',
+			company: {
+				name: 'My company',
+				catchPhase: 'Multi-layered client-server neural-net',
+				bs: 'harness real-time e-markets',
+			},
+		}
+
+		const newUserWithEmailIsAlreadyInUse = {
+			name: 'John Doe',
+			username: 'Doe',
+			email: 'johndoe@test.com',
+			address: {
+				street: 'Kulas Light',
+				suite: 'Apt. 10',
+				city: 'Teresina',
+				zipcode: '4567489-10',
+				geo: {
+					lat: '-37.4564',
+					lng: '-37.4564',
+				},
+			},
+			phone: '(086) 32904-7310',
+			website: 'github.com',
+			company: {
+				name: 'My company',
+				catchPhase: 'Multi-layered client-server neural-net',
+				bs: 'harness real-time e-markets',
+			},
+		}
+
+		await createUserService.execute(newUser)
+
+		await expect(
+			createUserService.execute(newUserWithEmailIsAlreadyInUse),
+		).rejects.toThrowError()
+		expect(spyFindOneByEmailFromFakeUserRepository).toHaveBeenCalledWith(
+			newUserWithEmailIsAlreadyInUse.email,
 		)
 	})
 })
