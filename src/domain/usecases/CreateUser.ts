e76ca7badd27724b6903507557d674fe11179629
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-19 17:22:52 -0300

    delete: createUser usecase

--- src/domain/usecases/CreateUser.ts
+++
@@ -1,9 +0,0 @@
-import { UserEntity } from '../user/UserEntity'
-
-export interface User extends UserEntity {
-	id: number
-}
-
-export interface AbstractCreateUser {
-	execute(data: UserEntity): Promise<User>
-}
