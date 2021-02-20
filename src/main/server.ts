10bc3a78ca8512b7cf60a122b13bceaa85bf8cf9
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-19 23:01:46 -0300

    refactor: use user/post router

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,39 +1,14 @@
 import 'reflect-metadata'
 import express from 'express'
 import '../infra/typeORM'
-import { PostRepositoryTypeORM } from '../infra/repository/typeORM/PostRepositoryTypeORM'
-import { CreatePostService } from '../data/post/services/CreatePostService'
-import { CreateUserService } from '../data/user/services/CreateUserService'
-import { UserRepositoryTypeORM } from '../infra/repository/typeORM/UserRepositoryTypeORM'
+import { userRouter } from './routes/UserRoutes'
+import { postRouter } from './routes/PostRoutes'
 
 const app = express()
 
 app.use(express.json())
 
-app.post('/user', async (request, response) => {
-	const userRequest = request.body
-
-	const userRepository = new UserRepositoryTypeORM()
-	const createUserService = new CreateUserService(userRepository)
-
-	const user = await createUserService.execute(userRequest)
-
-	return response.status(200).json(user)
-})
-
-app.post('/post', async (request, response) => {
-	const postRequest = request.body
-
-	const userRepository = new UserRepositoryTypeORM()
-	const postRepository = new PostRepositoryTypeORM()
-	const createPostService = new CreatePostService(
-		postRepository,
-		userRepository,
-	)
-
-	const post = await createPostService.execute(postRequest)
-
-	return response.status(200).json(post)
-})
+app.use('/user', userRouter)
+app.use('/post', postRouter)
 
 app.listen(3000, () => console.log('Server started at http://localhost:3000'))
