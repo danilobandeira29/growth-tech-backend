d55b3f3a3713bcd2a905769bd673d185dceaa5fe
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-19 21:31:09 -0300

    feat: add express server, add post/user routes

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,13 +1,39 @@
 import 'reflect-metadata'
-import { createServer, RequestListener } from 'http'
+import express from 'express'
+import '../infra/typeORM'
+import { PostRepositoryTypeORM } from '../infra/repository/typeORM/PostRepositoryTypeORM'
+import { CreatePostService } from '../data/post/services/CreatePostService'
+import { CreateUserService } from '../data/user/services/CreateUserService'
+import { UserRepositoryTypeORM } from '../infra/repository/typeORM/UserRepositoryTypeORM'
 
-const requestListener: RequestListener = (req, res) => {
-	res.writeHead(200)
-	res.end('Hello, World!')
-}
+const app = express()
 
-const httpServer = createServer(requestListener)
+app.use(express.json())
 
-httpServer.listen(3000, () =>
-	console.log('Server started at http://localhost:3000'),
-)
+app.post('/user', async (request, response) => {
+	const userRequest = request.body
+
+	const userRepository = new UserRepositoryTypeORM()
+	const createUserService = new CreateUserService(userRepository)
+
+	const user = await createUserService.execute(userRequest)
+
+	return response.status(200).json(user)
+})
+
+app.post('/post', async (request, response) => {
+	const postRequest = request.body
+
+	const userRepository = new UserRepositoryTypeORM()
+	const postRepository = new PostRepositoryTypeORM()
+	const createPostService = new CreatePostService(
+		postRepository,
+		userRepository,
+	)
+
+	const post = await createPostService.execute(postRequest)
+
+	return response.status(200).json(post)
+})
+
+app.listen(3000, () => console.log('Server started at http://localhost:3000'))
