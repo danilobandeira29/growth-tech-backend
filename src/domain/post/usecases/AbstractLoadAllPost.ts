ff992e58e0b4e392d96896675bd32d72a3f37223
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-20 11:59:10 -0300

    fix: return type AbstractLoadAllPost

--- src/domain/post/usecases/AbstractLoadAllPost.ts
+++ src/domain/post/usecases/AbstractLoadAllPost.ts
@@ -1,5 +1,10 @@
 import { Post } from './AbstractCreatePost'
+import { User } from '../../user/usecases/CreateUser'
+
+export interface PostWithUser extends Post {
+	user: User
+}
 
 export interface AbstractLoadAllPost {
-	execute(): Promise<Post[]>
+	execute(): Promise<PostWithUser[]>
 }
