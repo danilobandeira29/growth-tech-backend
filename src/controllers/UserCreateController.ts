3241527f441095244868390a22297db2774913dd
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-21 19:27:28 -0300

    feat: formats httpRequest for the database stricts

--- src/controllers/UserCreateController.ts
+++ src/controllers/UserCreateController.ts
@@ -1,4 +1,5 @@
 import { AbstractCreateUser } from '@/domain/user/usecases/CreateUser'
+import { UserHttpRequest } from './dtos/CreateUserHttpRequest'
 import { HttpRequest } from './dtos/HttpRequest'
 import { HttpResponse } from './dtos/HttpResponse'
 import { AbstractCreateController } from './interfaces/AbstractCreateController'
@@ -7,9 +8,29 @@ export class UserCreateController
 	implements AbstractCreateController<HttpRequest, HttpResponse> {
 	constructor(private readonly createUserService: AbstractCreateUser) {}
 
-	async create(data: HttpRequest): Promise<HttpResponse> {
+	async create(data: HttpRequest<UserHttpRequest>): Promise<HttpResponse> {
 		try {
-			const user = await this.createUserService.execute(data.body)
+			const formattedData = {
+				...data.body,
+				address: {
+					street: data.body.street,
+					suite: data.body.suite,
+					city: data.body.city,
+					zipcode: data.body.zipcode,
+					geo: {
+						lat: '99999',
+						lng: '99999',
+					},
+				},
+				website: '',
+				company: {
+					name: data.body.companyName,
+					catchPhase: 'catchPhase',
+					bs: 'bs',
+				},
+			}
+
+			const user = await this.createUserService.execute(formattedData)
 
 			return {
 				data: user,
