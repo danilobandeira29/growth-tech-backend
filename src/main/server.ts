452e0b61fb98857def10ddd2ec982f2489d270b4
Danilo Bandeira <danilobandeira29@gmail.com>
2021-02-21 19:28:00 -0300

    feat: add cors, change port

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,14 +1,16 @@
 import 'reflect-metadata'
+import cors from 'cors'
 import express from 'express'
 import '../infra/typeORM'
 import { userRouter } from './routes/UserRoutes'
 import { postRouter } from './routes/PostRoutes'
 
 const app = express()
+app.use(cors())
 
 app.use(express.json())
 
 app.use('/user', userRouter)
 app.use('/post', postRouter)
 
-app.listen(3000, () => console.log('Server started at http://localhost:3000'))
+app.listen(3333, () => console.log('Server started at http://localhost:3000'))
